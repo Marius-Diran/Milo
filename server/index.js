3b3393c099d1a9ad2c6de809ed7ab313945edbac
Marius Odediran <themariusodediran@gmail.com>
2025-05-06 19:03:57 +0100

    Making progress

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+require('dotenv').config();
+
+let initial_path = path.join(__dirname, "../app/src");
+
+const app = express();
+app.use(express.static(initial_path));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(initial_path, "../app/src/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
