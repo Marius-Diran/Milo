ee618578f73b1d7644ab40a596f803660f7e1d70
Marius Odediran <themariusodediran@gmail.com>
2025-05-08 03:32:58 +0100

    Progress

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-import { GoogleGenAI } from "@google/genai";
-
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
@@ -10,25 +8,15 @@ let initial_path = path.join(__dirname, "../app/src");
 const app = express();
 app.use(express.static(initial_path));
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(initial_path, "../app/src/index.html"));
 });
 
+// Gemini route
+app.use('/api/gemini', require("./gemini"));
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
-
-// API request
-const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
-
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
-
-await main();
