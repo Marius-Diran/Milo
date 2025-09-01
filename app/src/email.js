d27513a623912c78ed628d4c0c3ee1d244d288fe
Marius Odediran <themariusodediran@gmail.com>
2025-09-01 18:18:09 +0100

    New codes.

--- app/src/email.js
+++ app/src/email.js
@@ -1,4 +1,6 @@
 const emailInput = document.getElementById("email");
+const passwordIcon = document.querySelector(".fa-lock");
+let passwordInput = document.getElementById("password");
 
 const validDomains = [
   'gmail.com', 'yahoo.com', 'outlook.com', 'hotmail.com', 'icloud.com',
@@ -6,10 +8,26 @@ const validDomains = [
   'live.com', 'msn.com', 'yahoo.co.uk', 'gmail.co.uk', 'me.com'
 ];
 
+passwordIcon.onclick = function() {
+  if (passwordInput.type == "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
+
 function validateEmail(email) {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   if (!emailRegex.test(email)) {
     return { valid: false, message: "Please enter a valid email address."};
   }
+}
+
+const domain = email.split("@")[1]?.LowerCase();
+
+// check if the domain has at least 2 characters before TLD.
+const domainParts = domain.split(".");
+if (domainParts[0].length < 2) {
+  return { valid: false, message: "This domain name is too short" };
 }
